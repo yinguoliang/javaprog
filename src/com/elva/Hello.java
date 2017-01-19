c496cc4f9ff0d4d331ff6c672e8fc3c6422999a2
yinguoliang <yglah126@126.com>
2017-01-19 13:17:18 +0800

    hello

--- src/com/elva/Hello.java
+++ src/com/elva/Hello.java
@@ -2,14 +2,33 @@ package com.elva;
 
 
 
-
+class PPP{
+    private String value ;
+    public void setValue(String value){
+        this.value = value;
+    }
+    public String getValue(){
+        return value;
+    }
+}
 public class Hello {
+    public void deal(PPP p) throws Exception{
+        p = new PPP();
+        p.setValue("HHHHHH");
+        throw new Exception();
+    }
 	public void sayHello(){
-		System.out.println("Hello......every one");
+	    final PPP p = new PPP();
+	    try{
+	        deal(p);
+	    }catch(Exception ex){
+	        
+	    }
+	    System.out.println(p.getValue());
 	}
 	public static void main(String[] args) {
 	    
-	    
+	    new Hello().sayHello();
 	}
 
 }
