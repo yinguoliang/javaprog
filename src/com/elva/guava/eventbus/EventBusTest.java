ecffa5e2898a7a1de56de426c0b1e248cc2d74e9
yinguoliang <yglah126@126.com>
2017-02-08 15:24:21 +0800

    event bus

--- src/com/elva/guava/eventbus/EventBusTest.java
+++ src/com/elva/guava/eventbus/EventBusTest.java
@@ -1,5 +1,9 @@
 package com.elva.guava.eventbus;
 
+import java.util.concurrent.Executors;
+
+import com.google.common.eventbus.AllowConcurrentEvents;
+import com.google.common.eventbus.AsyncEventBus;
 import com.google.common.eventbus.EventBus;
 import com.google.common.eventbus.Subscribe;
 
@@ -15,23 +19,52 @@ public class EventBusTest {
 	}
 	static class EventListner {
 		@Subscribe
-		public void listen(Message msg){
+		public void listen(Message msg) throws Exception{
 			msg.msg=msg.msg+"11111";
-			System.out.println("hello "+msg);
+			Thread.sleep(2000);
+			System.out.println(Thread.currentThread().getName()+":hello "+msg);
 		}
+		/**
+		 * AllowConcurrentEvents : 该注解表示方法是线程安全的
+		 * 如果没有被AllowConcurrentEvents注解，guava在生成代理的时候，会加上synchronized关键字
+		 * 
+		 * @param msg
+		 * @throws Exception
+		 */
 		@Subscribe
-		public void welldone(Message msg){
+		@AllowConcurrentEvents
+		public void welldone(Message msg) throws Exception{
 			msg.msg=msg.msg+"2222";
-			System.out.println("well done "+msg);
+			Thread.sleep(2000);
+			System.out.println(Thread.currentThread().getName()+":well done "+msg);
 		}
 	}
 	public static void test1() throws Exception{
+	    //EventBus是同步的，及post和subcribe都是在同一个线程中执行的
+	    //如果要异步的话，可以使用AsyncEventBus， 见test2()
 		EventBus eventBus = new EventBus("test");
 		eventBus.register(new EventListner());
-		
+		System.out.println(Thread.currentThread().getName()+": post msg");
 		eventBus.post(new Message("HHHH"));
+		System.out.println(Thread.currentThread().getName()+": post msg");
+		eventBus.post(new Message("IIII"));
+		System.out.println(Thread.currentThread().getName()+": post msg");
+		eventBus.post(new Message("JJJJ"));
+	}
+	
+	public static void test2() throws Exception{
+	    //AsyncEventBus是异步的，继承于EventBus
+	    //修改了部分方法的实现
+	    AsyncEventBus eventBus = new AsyncEventBus("test",Executors.newSingleThreadExecutor());
+        eventBus.register(new EventListner());
+        System.out.println(Thread.currentThread().getName()+": post msg");
+        eventBus.post(new Message("HHHH"));
+        System.out.println(Thread.currentThread().getName()+": post msg");
+        eventBus.post(new Message("IIII"));
+        System.out.println(Thread.currentThread().getName()+": post msg");
+        eventBus.post(new Message("JJJJ"));
 	}
 	public static void main(String args[]) throws Exception{
-		test1();
+	    test1();
 	}
 }
