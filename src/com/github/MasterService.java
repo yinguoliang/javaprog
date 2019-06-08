bd49b4a2b4d3d69b877411a71034cd4bcbf0c4bd
yinguoliang <yglah126@126.com>
2019-06-08 09:21:01 +0800

    m

--- src/com/github/MasterService.java
+++ src/com/github/MasterService.java
@@ -2,7 +2,7 @@ package com.github;
 
 public class MasterService {
 	public static void main(String[] args) {
-		System.out.println("Hello main");
+		System.out.println("Hello main");//main
 		System.out.println("哈哈");
 		System.out.println("呵呵");
 		System.out.println("嘿嘿");
